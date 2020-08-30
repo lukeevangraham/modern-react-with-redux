8aae1cdefaa86b8824c9abb210fcb6452fee2629
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-29 21:21:53 -0700

    finished the seasons app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,30 +1,32 @@
 import React from "react";
-import ReactDOM, { render } from "react-dom";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  state = { lat: null, errorMessage: ''}
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
-    console.log("My component was rendered to the screen");
   }
 
-  componentDidUpdate() {
-    console.log("My component was just updated - it rerendered");
-  }
+  componentDidUpdate() {}
 
-  // requirement of react
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Lattitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading!</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
