447a94ab120f80a8d899a926d817a5b10e169af3
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-30 21:10:15 -0700

    working on videos project

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // todo:  make sure we call callback from parent component
+    this.props.onFormSubmit(this.state.term)
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} action="" className="ui form">
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
