00d35772c80b3f36f37de99ab9245c8ad08ac664
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-31 21:24:09 -0700

    approaching use of 'useRef'

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework'
+    title: "What is React?",
+    content: "React is a front end javascript framework",
   },
   {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers'
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
   },
   {
-    title: 'How do you use React?',
-    content: 'You use react by creating components'
-  }
-]
+    title: "How do you use React?",
+    content: "You use react by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
-  )
+  );
 };
