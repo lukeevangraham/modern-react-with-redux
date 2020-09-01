195da4c784ab9aa92958c0c1037ca8bbe6bad029
Cefalu Wave <luke.evan.graham@gmail.com>
2020-09-01 16:37:25 -0700

    dropdown complete?

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,29 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
-const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    });
+  }, []);
 
   const renderedOptions = options.map((option) => {
-
     if (option.value === selected.value) {
-        return null;
+      return null;
     }
 
     return (
-      <div key={option.value} onClick={() => onSelectedChange(option) } className="item">
+      <div
+        key={option.value}
+        onClick={() => onSelectedChange(option)}
+        className="item"
+      >
         {option.label}
       </div>
     );
   });
+
+  console.log(ref.current)
+
+
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div onClick={() => setOpen(!open)}
-        className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
