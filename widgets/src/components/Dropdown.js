adf7d199574fa04624f2c9abddd59cc0b4961447
Cefalu Wave <luke.evan.graham@gmail.com>
2020-09-01 23:21:24 -0700

    finished widgets app

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener("click", (event) => {
+    const onBodyClick = (event) => {
       if (ref.current.contains(event.target)) {
         return;
       }
       setOpen(false);
-    });
+    };
+
+    document.body.addEventListener("click", onBodyClick);
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
   }, []);
 
   const renderedOptions = options.map((option) => {
@@ -29,13 +35,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
 
-  console.log(ref.current)
-
-
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{label}</label>
         <div
           onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
