52ab2105bb37125fe0c1c965a1f92a3aa9a8c821
Cefalu Wave <luke.evan.graham@gmail.com>
2020-09-22 00:03:15 -0700

    first axios request for weather app working

---
+++ tinder/src/reducers/reducer_active_book.js
@@ -0,0 +1,12 @@
+// state arg is not app state, only the state this 
+// reducer is responsible for
+export default function(state = null, action) {
+    switch (action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+        default:
+            return state
+    }
+
+    
+}
